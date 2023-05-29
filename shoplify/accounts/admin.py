ee01dfec182d6699b1cdb20d12ae7c81fc2e9f33
sumitstha1 <edchristian.inventor@gmail.com>
2023-05-29 16:18:22 +0545

    delivery tracking added

--- shoplify/accounts/admin.py
+++ shoplify/accounts/admin.py
@@ -1,10 +1,48 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.admin import register
 
 
 # Register your models here.
 admin.site.register(UserProfile)
-admin.site.register(Cart)
-admin.site.register(CartItems)
+
+
+@register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_filter = ["is_paid", "user"]
+    ordering = ["is_paid"]
+    list_display = ["user", "is_paid"]
+
+@register(CartItems)
+class CartItemsAdmin(admin.ModelAdmin):
+    list_display = ["cart", "product"]
+    ordering = ["updated_at"]
+
+@register(PaidCart)
+class PaidCartAdmin(admin.ModelAdmin):
+    list_filter = ["is_delivered", "user"]
+    list_display = ["user", "is_delivered"]
+    ordering = ["updated_at"]
+
+@register(PaidCartItems)
+class PaidCartItemsAdmin(admin.ModelAdmin):
+    list_filter = ["cart"]
+    list_display = ["cart", "product"]
+    ordering = ["updated_at"]
+
+@register(DeliveredCart)
+class DeliveredCartAdmin(admin.ModelAdmin):
+    list_display = ["user", "is_delivered"]
+    ordering = ["updated_at"]
+
+@register(DeliveredCartItems)
+class DeliveredCartItemsAdmin(admin.ModelAdmin):
+    # list_display = ["product__product_name"]
+    # print("product__product_name")
+    list_display = ["cart","product"]
+    ordering = ["updated_at"]
+    list_filter = ["updated_at"]
+
+
 admin.site.register(WishList)
 admin.site.register(WishlistItems)
--- shoplify/accounts/models.py
+++ shoplify/accounts/models.py
@@ -49,11 +49,77 @@ class Cart(BaseModel):
                 return sum(price) - self.coupon.discount_price
 
         return sum(price)
+    
+class PaidCart(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='paid_carts')
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+    is_paid = models.BooleanField(default=True)
+    is_delivered = models.BooleanField(default=False)
 
+    def __str__(self) -> str:
+        return self.user.username
 
+    def get_total_price(self):
+        cart_items = self.cart_items.all()
+        price = []
+        for cart_item in cart_items:
+            price.append(cart_item.quantity * cart_item.product.selling_price)
+
+        if self.coupon:
+            if self.coupon.minimum_amount < sum(price):
+                return sum(price) - self.coupon.discount_price
+
+        return sum(price)
+    
+class DeliveredCart(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delivered_carts')
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+    is_paid = models.BooleanField(default=True)
+    is_delivered = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+    def get_total_price(self):
+        cart_items = self.cart_items.all()
+        price = []
+        for cart_item in cart_items:
+            price.append(cart_item.quantity * cart_item.product.selling_price)
+
+        if self.coupon:
+            if self.coupon.minimum_amount < sum(price):
+                return sum(price) - self.coupon.discount_price
+
+        return sum(price)
 
 class CartItems(BaseModel):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    color_variant = models.ForeignKey(ColorVariant, on_delete=models.SET_NULL, blank=True, null=True)
+    size_variant = models.ForeignKey(SizeVariant, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=1)
+
+    def get_product_price(self):
+        price = self.product.selling_price
+        quantity = self.quantity
+        price = price * quantity
+        return price
+
+class PaidCartItems(BaseModel):
+    cart = models.ForeignKey(PaidCart, on_delete=models.CASCADE, related_name='paid_cart_items')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    color_variant = models.ForeignKey(ColorVariant, on_delete=models.SET_NULL, blank=True, null=True)
+    size_variant = models.ForeignKey(SizeVariant, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=1)
+
+    def get_product_price(self):
+        price = self.product.selling_price
+        quantity = self.quantity
+        price = price * quantity
+        return price
+    
+class DeliveredCartItems(BaseModel):
+    cart = models.ForeignKey(DeliveredCart, on_delete=models.CASCADE, related_name='delivered_cart_items')
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     color_variant = models.ForeignKey(ColorVariant, on_delete=models.SET_NULL, blank=True, null=True)
     size_variant = models.ForeignKey(SizeVariant, on_delete=models.SET_NULL, blank=True, null=True)
@@ -88,5 +154,41 @@ def sent_email_token(sender, instance, created, **kwargs):
             send_email_activation_token(email, email_token)
     except Exception as e:
         print(e)
+
+@receiver(post_save, sender = Cart)
+def update_cart(sender, instance, created, **kwargs):
+    try:
+        if instance.is_paid:
+            paidcart, _ = PaidCart.objects.get_or_create(user = instance.user, is_paid = True)
+            cart_items = CartItems.objects.filter(cart = instance)
+            for cart_item in cart_items:
+                paid_cart_items = PaidCartItems.objects.create(cart = paidcart, product = cart_item.product, color_variant = cart_item.color_variant, size_variant = cart_item.size_variant, quantity = cart_item.quantity)
+                # print(cart_item.cart)
+
+            cart_items.delete()
+
+            if not cart_items:  # when the cart items is deleted then the instance is also deleted to prevent from deleting the instance before migrating the cart items to the paid cart items
+                instance.delete()
+
+    except Exception as e:
+        print(e)
+
+@receiver(post_save, sender = PaidCart)
+def update_delivered_cart(sender, instance, created, **kwargs):
+    try:
+        if instance.is_delivered:
+            deliveredcart, _ = DeliveredCart.objects.get_or_create(user = instance.user, is_paid = True, is_delivered=True)
+            cart_items = PaidCartItems.objects.filter(cart = instance)
+            print(cart_items)
+            for cart_item in cart_items:
+                delivered_cart_items = DeliveredCartItems.objects.create(cart = deliveredcart, product = cart_item.product, color_variant = cart_item.color_variant, size_variant = cart_item.size_variant, quantity = cart_item.quantity)
+            cart_items.delete()
+
+            if not cart_items:  # same as above
+                instance.delete()
+
+    except Exception as e:
+        print(e)
+
 
 
